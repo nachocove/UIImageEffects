8de3f75df3c8bdabd28aaf0d9dd6fd87d7c9d5ab
Steve Scalpone <sscalpone@gmail.com>
2014-01-27 23:09:24 -0800

    Initial check-in.

---
+++ UIImageEffectsBinding/UIImageEffectsBinding/ApiDefinition.cs
@@ -0,0 +1,29 @@
+using System;
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace UIImageEffectsBinding {
+
+	[Category, BaseType (typeof (UIImage))]
+	public partial interface ImageEffects_UIImage {
+
+        [Export ("applySubtleEffect")]
+        UIImage ApplySubtleEffect();
+
+        [Export ("applyLightEffect")]
+        UIImage ApplyLightEffect();
+
+        [Export ("applyExtraLightEffect")]
+        UIImage ApplyExtraLightEffect();
+
+        [Export ("applyDarkEffect")]
+        UIImage ApplyDarkEffect();
+
+		[Export ("applyTintEffectWithColor:")]
+		UIImage ApplyTintEffectWithColor (UIColor tintColor);
+
+		[Export ("applyBlurWithRadius:tintColor:saturationDeltaFactor:maskImage:")]
+		UIImage ApplyBlurWithRadius (float blurRadius, UIColor tintColor, float saturationDeltaFactor, UIImage maskImage);
+	}
+}
